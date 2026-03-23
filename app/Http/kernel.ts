fcfabd0c8b0406d7e31fc61fb249e4ec1104ecf0
jammehabdou64 <jammehabdou64@gmail.com>
2026-03-23 15:22:34 +0000

    upgrade packages

--- app/Http/kernel.ts
+++ app/Http/kernel.ts
@@ -1,9 +1,3 @@
-import cors from "cors";
-import session from "express-session";
-import cookieParser from "cookie-parser";
-import flash from "connect-flash";
-import fileUpload from "express-fileupload";
-import morgan from "morgan";
 import { auth, guest } from "jcc-express-mvc";
 import { inertia } from "jcc-express-mvc/Core/Inertia";
 
@@ -11,17 +5,7 @@ export class Kernel {
   //
 
   public middlewares = [
-    morgan("dev"),
-    cookieParser(),
-    cors(),
-    session({
-      secret: "ggggggg",
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 60000 },
-    }),
-    flash(),
-    fileUpload(),
+    // Global middlewares
     inertia({ rootView: "index" }),
   ];
 
